4eedb1f52bc2cc21efba3698694a1cf0be1a8534
= <kkimj@hanyang.ac.kr>
2021-06-02 13:22:03 +0900

    update testStyles #5

--- folio_flutter/lib/themes.dart
+++ folio_flutter/lib/themes.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:folio_flutter/styles.dart';
+
 
 ThemeData FolioLightTheme = ThemeData.light().copyWith(
   primaryColor: Color(0xFF264653),
   accentColor: Color(0xFF2a9d8f),
   appBarTheme: AppBarTheme(color: Color(0xFF264653)),
   textTheme: TextTheme(
-    headline6: TextStyle(color: Colors.black),
-    subtitle1: TextStyle(color: Colors.black),
-    subtitle2: TextStyle(color: Colors.black),
+    headline6: TextStyles.h1,
+    subtitle1: TextStyles.body1,
+    subtitle2: TextStyles.body2,
   ),
   textButtonTheme: TextButtonThemeData(),
   outlinedButtonTheme: OutlinedButtonThemeData(),
@@ -24,9 +26,9 @@ ThemeData FolioDarkTheme = ThemeData.dark().copyWith(
   accentColor: Color(0xFF2a9d8f),
   appBarTheme: AppBarTheme(color: Color(0xFF264653)),
   textTheme: TextTheme(
-    headline6: TextStyle(color: Colors.white),
-    subtitle1: TextStyle(color: Colors.white),
-    subtitle2: TextStyle(color: Colors.white),
+    headline6: TextStyles.h1.copyWith(color: Colors.white),
+    subtitle1: TextStyles.body1.copyWith(color: Colors.white),
+    subtitle2: TextStyles.body2.copyWith(color: Colors.white),
   ),
   textButtonTheme: TextButtonThemeData(),
   outlinedButtonTheme: OutlinedButtonThemeData(),
